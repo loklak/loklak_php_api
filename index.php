e75582003a4b7c0582bab25f1be98635e0a2821d
daminisatya <daminisatya@gmail.com>
2016-04-20 17:35:14 +0530

    Added peers response API to loklak-php[Structure]

--- index.php
+++ index.php
@@ -8,10 +8,15 @@ $l = new Loklak($baseURL);
 
 $values = $l->hello();
 $helloResponse = json_decode($values);
+$values = $l->peers();
+$peersResponse = json_decode($values);
 
 $bodyResponse = $helloResponse->body;
 $bodyResponse = json_decode($bodyResponse);
+$peersResponse = $peersResponse->body;
+$peersResponse = json_decode($peersResponse);
 
 echo "<b>Given Base URL  - </b>" . $baseURL . "<br>";
-echo "<b>Hello JSON Test - </b>" . $bodyResponse->status;
+echo "<b>Hello JSON Test - </b>" . $bodyResponse->status . "<br>";
+echo "<b>Peers JSON Test - </b>" . $peersResponse->count . " peers <br>";
 ?>
