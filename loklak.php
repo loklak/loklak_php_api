dda437752ddff425a638c4bc5f58a1a663e7c722
sopan khosla <khoslasopan@gmail.com>
2016-05-06 01:07:25 +0530

    Adds account API
    Fixes indentation issues
    
    Adds corrected headers to account post request

--- loklak.php
+++ loklak.php
@@ -186,4 +186,45 @@ class Loklak {
 			return json_encode($request, true);
 		}
 	}
+
+	public function account($name=null, $action=null, $data=null) {
+		$this->requestURL = $this->baseUrl . '/api/account.json';
+		$this->name = $name;
+		$this->action = $action;
+		$this->data = $data;
+		$headers = array('Accept' => 'application.json');
+		$headers['User-Agent'] = "Mozilla/5.0 (Android 4.4; Mobile; rv:41.0) Gecko/41.0 Firefox/41.0";
+		$headers['From'] = 'info@loklak.org';
+		if ($name) {
+			$params = array('screen_name'=>$this->name);
+			$request = Requests::request($this->requestURL, $headers, $params);
+			if ($request->status_code == 200)
+				return json_encode($request, true);
+			else {
+				$request = array();
+				$error = "Something went wrong, looks like the query is wrong.";
+				$request['error'] = array_push($request, $error);
+				return json_encode($request, true);
+			}
+		}
+		else if ($this->action == "update" && $data) {
+			$params['action'] = $this->action;
+			$params['data'] = $this->data;
+			$request = Requests::request($this->requestURL, $headers, $params);
+			if ($request->status_code == 200)
+				return json_encode($request, true);
+			else {
+				$request = array();
+				$error = "Something went wrong, looks like the server is down.";
+				$request['error'] = array_push($request, $error);
+				return json_encode($request, true);
+			}
+		}
+		else {
+			$request = array();
+			$error = "No Query string has been given to run a query for account.	";
+			$request['error'] = array_push($request, $error);
+			return json_encode($request, true);
+		}
+	}
 }
