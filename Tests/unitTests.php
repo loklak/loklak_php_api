49fb60fd0b23e9dff7aa7b4b5e985a2a104fe532
sopan khosla <khoslasopan@gmail.com>
2016-05-07 14:44:36 +0530

    Adds test function for status

--- Tests/unitTests.php
+++ Tests/unitTests.php
@@ -9,4 +9,23 @@ class Testloklak extends \PHPUnit_Framework_TestCase
     public function setUp() {
         $this->loklak = new Loklak();
     }
+
+    public function testStatus() {
+        $result = $this->loklak->status();
+        $statusResponse = json_decode($result);
+        $statusResponse = $statusResponse->body;
+        $statusResponse = json_decode($statusResponse, true);
+        $this->assertArrayHasKey('indexes', $statusResponse);
+
+        $resultProperties = array(
+            'messages', 'mps', 'users', 'queries',
+            'accounts', 'user', 'followers', 'following'
+        );
+
+        foreach($resultProperties as $property)
+        {
+            $error = "Indexes does not contain " . $property;
+            $this->assertArrayHasKey($property, $statusResponse['indexes'], $error);
+        }
+    }
 }
